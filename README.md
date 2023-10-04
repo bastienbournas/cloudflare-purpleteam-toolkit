db2743d9432a73709e9f7585425b3163600fa774
Bastien Bournas <bbournas@sophiagenetics.com>
2023-10-04 11:20:23 +0200

    Implement bypass threw cloudflare DNS misconfiguration in cf_waf_bypass.py

--- README.md
+++ README.md
@@ -3,28 +3,52 @@
 This repo contains a collection of tools to perform some security tests, scans and investigations on the established configurations in cloudflare. To keep everything simple, each of them is a small python script with a reduced scope.
 
 The toolkit contains:
-1. cf-waf-bypass - try to bypass the WAF on your origin
-2. cf-dns-detective - enumerate and investigate dns entries, including the proxy status
+1. cf_waf_bypass - try to bypass the WAF on your origin
+2. cf_dns_map - enumerate dns entries, including the proxy status
 3. cf-zt-gateway-detective - search, enumerate and investigate among the Zero Trust Gateway policies
 
-### 1. cf-waf-bypass
-This tool tries to bypass cloudflare by accessing directly to the origin server IP. 2 bypasses are currently implemented.
+### 1. cf_waf_bypass
+This tool aims to bypass cloudflare by accessing directly to the origin server IP, escaping the WAF analysis. 3 kind of bypasses are currently implemented.
 
 1. direct bypass
 
-Sometimes, cloudflare proxy is enabled for a domain, but there is no actual restriction on the server to prevent traffic from coming directly. When a "normal" user try to reach the service, cloudflare DNS will answer with the IP of its own proxy, and will forward traffic to the origin. But if a bad actor knows the IP address, he can use it directly and hit the server without going threw the cloudflare proxy, and the waf rules. Very often the origin IP can easily be found in archives or search engines such as shodan. The cf-waf-bypass tool attempts to do exactly this bypass, by takin in parameter the url, and the origin ip. It'll print the answer, so that you know if the bypass was successful
+Sometimes, cloudflare proxy is enabled for a domain, but there is no actual restriction on the server to prevent traffic from coming directly. When a "normal" user try to reach the service, cloudflare DNS will answer with the IP of its own proxy, and will forward traffic to the origin. But if a bad actor knows the IP address, he can use it directly and hit the server without going threw the cloudflare proxy, and the waf rules. Very often the origin IP can easily be found in archives or search engines such as shodan. The cf_waf_bypass tool with the --direct_bypass option attempts to do exactly this bypass, by takin in parameter the url, and the origin ip. It'll print the answer, so that you know if the bypass was successful
 ```
-python3 cf-waf-bypass.py --direct -x GET -url https://my-target.com/foo.php -ip 6.6.6.6
+python3 cf_waf_bypass.py --direct_bypass -x GET -url https://my-target.com/foo.php -ip 6.6.6.6
 ```
 
-1. bypass cloudflare threw cloudflare
+2. bypass threw cloudflare DNS misconfiguration
+
+Misconfigurations in cloudflare dns itself can lead to proxied entries bypass. Some example of such misconfigurations are an unproxified CNAME entry pointing to a proxified entry, or a different unproxified entry with same IP as a proxified one. Using the cf_waf_bypass tool with the --dns_bypass option allows to scan all the dns entries threw the cloudflare API, and outputs a list of all the possibles bypasses caused by these misconfigurations. 
+The output is in CSV format, each raw containing : ["proxied_type","proxied_name","proxied_content","bypass_type","bypass_name","bypass_content"]. proxied_* represents the attributes of the proxied entry that can be bypassed, and bypass_* represents the attributes of the entry that allows the bypass. When a bypass is found, you can then use --direct_bypass option to actually execute it.
+```
+python3 cf_waf_bypass.py --dns_bypass -cft CF_API_TOKEN --zid CF_ZONE_ID -o output.csv
+```
+
+3. bypass cloudflare threw cloudflare
 
 TODO
 https://certitude.consulting/blog/en/using-cloudflare-to-bypass-cloudflare/
 
-### 2. cf-dns-detective
+### 2. cf_dns_map
 
-TODO
+Threw cloudflare API, cf_dns_map allows to enumerate dns entries, including the proxy status.
+The output is in CSV format, so that it's easy to process. 
+
+List all dns entries with proxy status:
+```
+python3 cf_dns_map.py -zid CF_ZONE_ID -cft CF_API_TOKEN -o output.csv
+```
+
+List proxied dns entries:
+```
+python3 cf_dns_map.py --proxied -zid CF_ZONE_ID -cft CF_API_TOKEN -o output.csv
+```
+
+List unproxied dns entries:
+```
+python3 cf_dns_map.py --unproxied -zid CF_ZONE_ID -cft CF_API_TOKEN -o output.csv
+```
 
 ### 3. cf-zt-gateway-detective
 
---
+++ cf_waf_bypass.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+import sys
+import requests
+import ipaddr
+from forcediphttpsadapter.adapters import ForcedIPHTTPSAdapter
+import argparse
+import csv
+from cf_purpleteam_toolkit_common import is_ipv4
+from cf_dns_map import get_cf_dns_entries
+
+#   
+# Bypass cloudflare with the server orgin IP.
+# Url is used to forge the appropriate host header, but the request is sent directly to the IP address.
+# ForcedIPHTTPSAdapter is used to make the ssl scheme work (https).
+#
+def bypass_cloudflare_directly(url, ip):
+    session = requests.Session()
+    session.mount(url, ForcedIPHTTPSAdapter(dest_ip=ip))
+    r = session.get(url, verify=False)
+    print(r.text)
+
+#   
+# For the given proxied DNS entry name and IP, check for bypasses in the unproxied_entries list
+#
+def check_for_proxy_bypass(proxied_name, proxied_ip, unproxied_entries):
+    bypass_list = []
+    for unproxied_entry in unproxied_entries:
+        if unproxied_entry[0] == "A":
+            if is_ipv4(unproxied_entry[2]):
+                ip = unproxied_entry[2]
+                if proxied_ip == ip:
+                    bypass_list.append(unproxied_entry)
+        elif unproxied_entry[0] == "CNAME":
+            cname_alias = unproxied_entry[2]
+            if proxied_name == cname_alias:
+                bypass_list.append(unproxied_entry)
+
+    return bypass_list
+
+#   
+# Search for bypass threw DNS itself (unproxified CNAME, different unproxified entry with same IP, etc)
+# Returns a list of DNS entries that allow to bypass a proxied DNS entry, following this format:
+#   ["proxied_type","proxied_name","proxied_content","bypass_type","bypass_name","bypass_content"]
+# Where proxied_* represents the attributes of the proxied entry that can be bypassed,
+# and bypass_* represents the attributes of the entry that allows the bypass
+#
+def get_cf_bypassing_dns_entries(cf_token, zone_identifier):
+    proxied_entries = get_cf_dns_entries(cf_token, zone_identifier, True)
+    unproxied_entries = get_cf_dns_entries(cf_token, zone_identifier, False)
+    
+    output = []
+    output.append(["proxied_type","proxied_name","proxied_content","bypass_type","bypass_name","bypass_content"])
+    for proxied_entry in proxied_entries:
+        if (is_ipv4(proxied_entry[2])):
+            name = proxied_entry[1]
+            ip = proxied_entry[2]
+            bypass_list = check_for_proxy_bypass(name, ip, unproxied_entries)
+            for bypass in bypass_list:
+                output.append([proxied_entry[0], proxied_entry[1], proxied_entry[2], bypass[0],bypass[1],bypass[2]])
+
+    return output
+
+#
+# Argument parsing for CLI usage
+#
+def parse_args():
+    parser = argparse.ArgumentParser("cloudflare_waf_bypass")
+    parser.add_argument('-d','--direct_bypass', required=False, action='store_true', help="Bypass cloudflare with the server orgin IP. Use with -url and -ip")
+    parser.add_argument("-url","--url",  required=('--direct' in sys.argv) or ('-d' in sys.argv), help="Url to make the test request", type=str)
+    parser.add_argument("-ip","--ip", required=('--direct' in sys.argv) or ('-d' in sys.argv), help="Origin IP address to bypass cloudflare proxy", type=str)
+    parser.add_argument("-x","--request_type", required=False, help="GET or POST (GET is the default)", type=str, default="GET")
+    parser.add_argument('-dns','--dns_bypass', required=False, action='store_true', help="Search for bypass threw DNS itself (unproxified CNAME, different unproxified entry with same IP, etc). Needs cloudflare API token: use with -cft and -zid options")
+    parser.add_argument("-cft","--cf_token", required=('--dns_bypass' in sys.argv) or ('-dns' in sys.argv), help="Cloudflare Authorization Bearer token", type=str)
+    parser.add_argument("-zid","--zone_id", required=('--dns_bypass' in sys.argv) or ('-dns' in sys.argv), help="Cloudflare zone identifier", type=str)
+    parser.add_argument("-o","--output", required=False, help="Dump all WAF informations to the given file", type=str)
+
+    if len(sys.argv)==1:
+        parser.print_help()
+        parser.exit()
+
+    return parser, parser.parse_args()
+
+#
+# Main
+#
+def main():
+    parser, args = parse_args()
+    if args.direct_bypass: 
+        bypass_cloudflare_directly(args.url,args.ip)
+    elif args.dns_bypass:
+        output = get_cf_bypassing_dns_entries(args.cf_token,args.zone_id)
+        if (args.output == None):
+            writer = csv.writer(sys.stdout)
+            writer.writerows(output)
+        else:
+            if args.output != None:
+                with open(args.output, 'w', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerows(output)
+    else:
+        parser.print_help()
+        parser.exit()
+
+if __name__ == "__main__":
+    main()
