04c7b7ddba3e63540bc8b146e8b62148be05856c
Bastien Bournas <bbournas@sophiagenetics.com>
2023-10-05 09:34:35 +0200

    Add some comments

--- cf_purpleteam_toolkit_common.py
+++ cf_purpleteam_toolkit_common.py
@@ -2,6 +2,17 @@
 
 import ipaddr
 
+############################################################################################################
+#                                                                                                          #
+# cf_purpleteam_toolkit_common.py                                                                          #
+#                                                                                                          #
+# This module regroups all the common code and helper functions to be shared by the tools.                 #
+#                                                                                                          #
+############################################################################################################
+
+#
+# Check that the fiven string respects the IPv4 format
+#
 def is_ipv4(string):
     try:
         ipaddr.IPv4Network(string)
